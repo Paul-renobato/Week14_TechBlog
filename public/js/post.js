870a734bdd9ba2e95b455389a64dd1d7005ecc92
paul <paul.renobato@gmail.com>
2021-08-15 23:34:18 -0500

    added the connection js file.

---
+++ public/js/post.js
@@ -0,0 +1,21 @@
+const newPost = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const body = document.querySelector('input[name="post-body"]').value;
+    
+        const response = await fetch(`/api/posts`, {
+            method: 'POST',
+            body: JSON.stringify({
+                "title": title,
+                "body": body
+            }),
+            headers: {'Content-Type': 'application/json'},
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        }
+};
+
+document.querySelector('.new-project-form').addEventListener('submit', newPost);
